e5b44876208e6ef056a9d0a4ef85e11ffbacdc54
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-04-13 17:14:31 +0300

    adding react

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [newExpense, setNewExpense] = useState({ name: "", description: "", amount: "" });
   const [sortBy, setSortBy] = useState("");
+  React.useEffect(() => {
+    const savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
+    setExpenses(savedExpenses);
+  }, []);
+
+  React.useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
  
 }
 
