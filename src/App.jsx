0ed375f1a5ce565e11f11e0cdc549c20fc28fd28
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-04-13 17:13:49 +0300

    adding constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
 function App() {
-  
+  const [expenses, setExpenses] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [newExpense, setNewExpense] = useState({ name: "", description: "", amount: "" });
+  const [sortBy, setSortBy] = useState("");
+ 
 }
 
 export default App
